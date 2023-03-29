c234cb406fdf91ccce0e493c22dad53f694a1036
THANT SIN HEIN <107390346+Thant-Sin-Hein@users.noreply.github.com>
2023-03-29 14:39:36 +0700

    Add files via upload

--- Tutorial_02/index.php
+++ Tutorial_02/index.php
@@ -9,113 +9,48 @@
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <h1>Diamond Pattern</h1>
-     <?php
+<h1>Diamond Pattern</h1>
+<?php
+ 
+     
     function makeDiamondShape($row)
     {
-        if ( $row == 0 ) {
-            echo $row. " parameter must be greather than 0";
-        }
-        elseif ( $row == 1 ) {
-            echo "<div style='text-align:center;margin-top:50px;'>
-            <span> * </span>
-            </div>";
-        }
-        elseif ( $row == 3 ) {
-            echo "<div style='text-align:center;margin-top:50px;'>
-            <span> * </span>
-            <span> *** </span>
-            <span> * </span>
-            </div>";
-        }
-        elseif ( $row == 5 ) {
-            echo "<div style='text-align:center;margin-top:50px;'>
-            <span> * </span>
-            <span> *** </span>
-            <span> ***** </span>
-            <span> *** </span>
-            <span> * </span>
-            </div>";
-        }
-        elseif ( $row == 7 ) {
-            echo "<div style='text-align:center;margin-top:50px;'>
-            <span> * </span>
-            <span> *** </span>
-            <span> ***** </span>
-            <span> ******* </span>
-            <span> ***** </span>
-            <span> *** </span>
-            <span> * </span>
-            </div>";
-        }
-        elseif ( $row == 9 ) {
-            echo "<div style='text-align:center;margin-top:50px;'>
-            <span> * </span>
-            <span> *** </span>
-            <span> ***** </span>
-            <span> ******* </span>
-            <span> ********* </span
-            <span> ******* </span>
-            <span> ***** </span>
-            <span> *** </span>
-            <span> * </span>
-            </div>";
-        }
-        elseif ( $row == 11 ) {
-            echo "<div style='text-align:center;margin-top:50px;'>
-            <span> * </span>
-            <span> *** </span>
-            <span> ***** </span>
-            <span> ******* </span>
-            <span> ********* </span>
-            <span> *********** </span>
-            <span> ********* </span>
-            <span> ******* </span>
-            <span> ***** </span>
-            <span> *** </span>
-            <span> * </span>
-            </div>";
-        }
-        elseif ( is_string($row) ) {
+        
+        if ( is_string($row) ) {
             echo "<p class='bg'>row parameter must be number</p>";
         }
-        elseif ( $row > 11 ) {
-            echo "<p class='bg'>row parameter must be less than 11</p>";
+        elseif ( is_int($row) ) {
+            echo "<div style='text-align:center;margin-top:50px;'>";
+            for ($i=1; $i <= $row; $i++) { 
+                for ($j=1; $j <= (2*$row)-1 ; $j++) { 
+                    if ($j>=$row-($i-1) && $j<=$row+($i-1)) {
+                        echo "*";
+                    }else {
+                        echo "&nbsp;&nbsp";
+                    }
+                }
+                echo '<br>';
+            }
+              for ($i=$row-1; $i <= $row; $i--) { 
+                for ($j=1; $j <= (2*$row)-1 ; $j++) { 
+                    if ($j>=$row-($i-1) && $j<=$row+($i-1)) {
+                        echo "*";
+                    }else {
+                        echo "&nbsp;&nbsp";
+                    }
+                }
+                echo '<br>';
+            }
+            echo "</div>";
         }
-        elseif ( $row = 2 ) {
-            echo "<p class='bg'>row parameter must be odd number</p>";
-        }
-        elseif ( $row = 4 ) {
-            echo "<p class='bg'>row parameter must be odd number</p>";
-        }
-        elseif ( $row = 6 ) {
-            echo "<p class='bg'>row parameter must be odd number</p>";
-        }
-        elseif ( $row = 8 ) {
-            echo "<p class='bg'>row parameter must be odd number</p>";
-        }
-        elseif ( $row = 10 ) {
-            echo "<p class='bg'>row parameter must be odd number</p>";
-        }
-      
         else {
             echo "<p class='bg'>Somethig Wrong</p>";
         }
 
     }
-    //makeDiamondShape(0);
-    //makeDiamondShape(1);
-    //makeDiamondShape(2);
-    //makeDiamondShape(6);
-    //makeDiamondShape(3);
-    //makeDiamondShape(5);
-    //makeDiamondShape(7);
-    //makeDiamondShape(9);
-    //makeDiamondShape(10);
-    makeDiamondShape(11);
-    //makeDiamondShape(12);
-    //makeDiamondShape('mgmg');
-    ?>
+
+    makeDiamondShape(5);
+?>
     
 </body>
 </html>
