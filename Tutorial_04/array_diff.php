9144bf9f6e448c6f2d17c46e081996e6b38caf5d
THANT SIN HEIN <107390346+Thant-Sin-Hein@users.noreply.github.com>
2023-03-30 09:00:24 +0700

    Add files via upload

--- Tutorial_04/array_diff.php
+++ Tutorial_04/array_diff.php
@@ -4,26 +4,12 @@
         if ((int)$arr1[0]!=(int)$arr2[0] && (int)$arr1[0]!=(int)$arr2[1] && (int)$arr1[0]!=(int)$arr2[2]) {
             echo $arr1[0];
         }
-        
         if ((int)$arr1[1]!=(int)$arr2[0] && (int)$arr1[1]!=(int)$arr2[1] && (int)$arr1[1]!=(int)$arr2[2]) {
             echo $arr1[1];
         }
         if ((int)$arr1[2]!=(int)$arr2[0] && (int)$arr1[2]!=(int)$arr2[1] && (int)$arr1[2]!=(int)$arr2[2]) {
             echo $arr1[2];
         }
-  
- 
-
     }
     arrayDiff([1, 2], [1]);
-    echo "<br>";
-    arrayDiff([1, 2, 2], [1]);
-    echo "<br>";
-    arrayDiff([1, 2, 2], [2]);
-    echo "<br>";
-    arrayDiff([1, 2, 2], []);
-    echo "<br>"; 
-    arrayDiff([], [1, 2]);
-    echo "<br>"; 
-    arrayDiff([1, 2, 3], [1, 2]);
 ?>
--- Tutorial_04/create_phone_number.php
+++ Tutorial_04/create_phone_number.php
@@ -1,14 +1,9 @@
 <?php
     function createPhoneNumber($numberArray)
     {
- 
-
     $firstThreeNumber=$numberArray[0].$numberArray[1].$numberArray[2];
     $secondThreeNumber=$numberArray[3].$numberArray[4].$numberArray[5];
     $thirdFourNumber=$numberArray[6].$numberArray[7].$numberArray[8].$numberArray[9];
-
         echo "output =>"."(".$firstThreeNumber.")"." "." ".$secondThreeNumber."-".$thirdFourNumber;
     }
     createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]); 
-    echo "<br>";
-    createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]); 
